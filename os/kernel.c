3dd8f83b08d61522eadbc907c820491279b0f18d
pedrogao <1312342604@qq.com>
2022-01-31 16:24:18 +0800

    feat: 02 print hello world

--- os/kernel.c
+++ os/kernel.c
@@ -1,5 +1,11 @@
+extern void uart_init(void);
+extern void uart_puts(char *s);
+
 void start_kernel(void)
 {
+    uart_init();
+    uart_puts("Hello tinyos!\n");
+
     while (1)
     {
     }; // 死循环，进入内核后啥也不干
