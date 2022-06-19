7dba69bcf49b176b246b22cfaf398ead20be040c
pedrogao <1312342604@qq.com>
2022-06-19 15:18:52 +0800

    feat: ch7 timer

---
+++ riscvos/7-hw-timer/timer.c
@@ -0,0 +1,30 @@
+#include "os.h"
+
+// 1s
+#define TIMER_INTERVAL CLINT_TIMEBASE_FREQ
+
+static uint32_t _tick = 0;
+
+void timer_load(int interval)
+{
+    int id = r_mhartid();
+
+    *(uint64_t *)CLINT_MTIMECMP(id) = *(uint64_t *)CLINT_MTIME + interval;
+}
+
+void timer_init()
+{
+    timer_load(TIMER_INTERVAL);
+
+    /* enable machine-mode timer interrupts. */
+    w_mie(r_mie() | MIE_MTIE);
+    /* enable machine-mode global interrupts. */
+    w_mstatus(r_mstatus() | MSTATUS_MIE);
+}
+
+void timer_handler()
+{
+    _tick++;
+    printf("tick: %d\n", _tick);
+    timer_load(TIMER_INTERVAL);
+}
---
+++ riscvos/7-hw-timer/trap.c
@@ -0,0 +1,75 @@
+#include "os.h"
+
+extern void trap_vector(void);
+extern void uart_isr(void);
+extern void timer_handler(void);
+
+void trap_init()
+{
+    // 设置 trap 处理函数
+    w_mtvec((reg_t)trap_vector);
+}
+
+// 外部中断处理函数，比如 uart
+void external_interrupt_handler()
+{
+    int irq = plic_claim();
+
+    if (irq == UART0_IRQ)
+    {
+        uart_isr();
+    }
+    else if (irq)
+    {
+        printf("unexpected interrupt irq = %d\n", irq);
+    }
+
+    if (irq)
+    {
+        plic_complete(irq);
+    }
+}
+
+reg_t trap_handler(reg_t epc, reg_t cause)
+{
+    reg_t return_pc = epc;
+    reg_t cause_code = cause & 0xfff;
+    if (cause & 0x80000000)
+    {
+        // 异步 trap，interrupt，中断
+        switch (cause_code)
+        {
+        case 3:
+            uart_puts("software interruption!\n"); // 软件中断
+            break;
+        case 7:
+            uart_puts("timer interruption!\n"); // 时钟中断
+            timer_handler();
+            break;
+        case 11:
+            uart_puts("external interruption!\n"); // 外设中断
+            external_interrupt_handler();
+            break;
+        default:
+            uart_puts("unknown async exception!\n");
+            break;
+        }
+    }
+    else
+    {
+        // 同步 trap，exception，异常
+        printf("sync exceptions!, code = %d\n", cause_code);
+        panic("can't go here!");
+    }
+
+    // 返回原始pc
+    return return_pc;
+}
+
+void trap_test()
+{
+    // 访问非法内存，会进入异常
+    *(int *)0x00000000 = 100;
+
+    uart_puts("Yeah! I'm return back from trap!\n");
+}
